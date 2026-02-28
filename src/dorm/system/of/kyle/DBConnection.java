1e869f98885c72e3a8d4434d03eb619af66e844a
buildwithjaym <jaymmaruji@gmail.com>
2026-02-28 19:16:03 +0800

    Added the files

---
+++ src/dorm/system/of/kyle/DBConnection.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dorm.system.of.kyle;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author admin
+ */
+public class DBConnection {
+    private Connection conn;
+
+    public DBConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+           System.out.println("Database successfully connected");
+        }
+
+        String url = "jdbc:mysql://localhost:3306/dorm_db?useSSL=false&serverTimezone=UTC";
+        String user = "root";
+        String password = "";
+
+        this.conn = DriverManager.getConnection(url, user, password);
+    }
+
+    public Connection getConnection() {
+        return this.conn;
+    }
+
+    public void close() throws SQLException {
+        if (conn != null && !conn.isClosed()) {
+            conn.close();
+        }
+    }
+}
+
+
+
+
+
+
